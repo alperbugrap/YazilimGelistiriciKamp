ef4c27fd33b4963c4d7ab3818d76a82fa16cddbb
ALPER BUĞRA POLAT <202802052@ogr.cbu.edu.tr>
2022-10-26 16:06:18 +0300

    261022

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> { "Murat", "Kerem", "Halil"};
+            
+            isimler2.Add("Engin");
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Alper");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Işıl");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
